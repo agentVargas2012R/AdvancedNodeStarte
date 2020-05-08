ce5780e97489a4d1de8f88564e0212d399176057
mvargas042717 <mvargas@redzonemap.com>
2020-05-07 21:09:28 -0400

    fix security issues

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '885077763258-co1cmpoqc5i5r5t0kcg1v83s9e6qcc9d.apps.googleusercontent.com',
-  googleClientSecret: 'g2PPVSdu8cPynNr_dF2TuzXi',
-  mongoURI: 'mongodb://mvargas:mike123@cluster0-shard-00-00-ulqzi.mongodb.net:27017,cluster0-shard-00-01-ulqzi.mongodb.net:27017,cluster0-shard-00-02-ulqzi.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
